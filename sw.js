a061bdafc46f0655aa5cc6670c32b36ec5444e8f
Gilberto Moreira <escolateologicavance@gmail.com>
2026-02-20 07:55:18 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'gestao-familiar-v3';
+
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './1.html',
+  './2.html',
+  './3.html',
+  './4.html',
+  './5.html',
+  './6.html',
+  './7.html',
+  './8.html',
+  './manifest.json',
+  './logo.png',
+  './icone-192.png',
+  './icone-512.png'
+];
+
+// INSTALAÇÃO
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// ATIVAÇÃO (limpa caches antigos automaticamente)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH (cache primeiro, depois rede)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
